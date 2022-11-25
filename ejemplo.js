97e7a8aa8223a0982190b6beac7052c217b881b8
cristhian-chavez <crsth.1990@fpuna.edu.py>
2022-11-25 20:48:33 -0300

    Ejercicio 7

--- ejemplo.js
+++
@@ -1,4 +0,0 @@
-let num = [1, 2, 3, 'una cosa', 5]
-for( let i = 0; i < num.length ; i++){
-    console.log(`i es ${i} y num es ${ num[i] }`)
-}
---
+++ ejercicio7/index.js
@@ -0,0 +1,17 @@
+let receta = {};
+
+receta.nombre = 'sandiwch';
+
+receta.ingredientes = [];
+
+receta.ingredientes.push({
+    nombre: 'pan',
+    cantidad: 2
+});
+
+receta.ingredientes.push({
+    nombre: 'queso',
+    cantidad: 1
+});
+
+console.log(receta)
